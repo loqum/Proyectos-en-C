a972f0c0e3bbd7861d14179201ef948e7a9aada6
ruben <ruben693@gmail.com>
2019-09-06 09:30:22 +0200

    Update parametrosPorReferencia.cpp
    Corregido.

--- parametrosPorReferencia.cpp
+++ parametrosPorReferencia.cpp
@@ -2,19 +2,21 @@
 
 using namespace std;
 
-void suma(int n1, int n2);
+void valNuevo(int&, int&);
 
 int main() {
 	
 	int n1, n2;
 	
 	cout << "Introduce dos numeros: "; cin >> n1 >> n2;
+	
+	valNuevo(n1, n2);
 		
 	return 0;
 }
 
 
-void valNuevo(int& n1, int& n2) {
-	cout << "El valor del primer numero es: " << n1 << endl;
-	cout << "El valor del segundo numero es: " << n2 << endl;
+void valNuevo(int& xnum, int& ynum) {
+	cout << "El valor del primer numero es: " << xnum << endl;
+	cout << "El valor del segundo numero es: " << ynum << endl;
 }
